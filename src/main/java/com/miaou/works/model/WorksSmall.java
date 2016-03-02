cc6e95dec06df5135471430a9b7e4069538f9523
YeeL <yee.lui7@gmail.com>
2016-03-02 14:53:14 +0100

    Commit test

---
+++ src/main/java/com/miaou/works/model/WorksSmall.java
@@ -0,0 +1,34 @@
+package com.miaou.works.model;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@DiscriminatorValue("SMALL")
+@Table(name = "works_small", catalog = "test")
+public class WorksSmall extends Works {
+    protected static final String ROLE = "ROLE_SMALL";
+    
+    private int cost; 
+
+    public WorksSmall(String title, String contents){
+        super(title, contents);
+    }
+
+    @Column(name = "cost")
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+    
+    @Transient
+    public String getRoles(){
+        return ROLE;
+    }
+}
